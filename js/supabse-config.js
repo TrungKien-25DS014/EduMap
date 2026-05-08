d1e6edc9071633dcfbdc8be01cebb844128f20b1
nobbutlovecode <toilanguoivietnam.317@gmail.com>
2026-05-09 06:46:23 +0700

    Update supabse-config.js

--- js/supabse-config.js
+++ js/supabse-config.js
@@ -1,7 +1,9 @@
 // js/supabase-config.js
+const supabaseUrl = 'URL_CỦA_BẠN';
+const supabaseKey = 'ANON_KEY_CỦA_BẠN';
 
-const supabaseUrl = 'URL_CỦA_BẠN_Ở_ĐÂY';
-const supabaseKey = 'ANON_KEY_CỦA_BẠN_Ở_ĐÂY';
+// Khởi tạo client duy nhất cho toàn bộ dự án
+const _supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
-// Khởi tạo thư viện Supabase
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+// Xuất ra biến toàn cục để các file js khác sử dụng
+window.supabaseClient = _supabase;
