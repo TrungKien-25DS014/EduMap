069ab7f189f4c64ffd68429eca58cdaa8f20c552
TrungKien-25DS014 <Phantrungk81@gmail.com>
2026-05-11 13:21:10 +0700

    Kien

---
+++ js/user-info.js
@@ -0,0 +1,28 @@
+const UserInfo = {
+    getUserID: () => {
+        return localStorage.getItem('userID');
+    },
+
+    isLoggedIn: () => {
+        return !!localStorage.getItem('userID');
+    },
+
+    getUserProfile: async () => {
+        const userID = UserInfo.getUserID();
+        if (!userID) return null;
+
+        try{
+            const { data, error } = await supabase
+                .from('users')
+                .select('*')
+                .eq('id', userID)
+                .Single();
+            if (error) throw error;
+            return data;
+        }catch(e) {
+            console.error("Lỗi khi lấy thông tin người dùng:", e.message);
+            return null;
+        }
+    }
+};
+window.UserInfo = UserInfo;
